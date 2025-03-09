894b224d308f283f8159c48fb5b1c79af116a2b0
sohidul556 <shamim5567@gmail.com>
2025-03-10 01:56:04 +0600

    js note

---
+++ nested.js
@@ -0,0 +1,20 @@
+const money = -10;
+if (money>300){
+    console.log("bro you are rich");
+}
+else{
+    if(money>100){
+        console.log('tui gorib ona boro lok na')
+
+    }
+    else{
+        if(money>0){
+            console.log("dostho kola ka calcime bara")
+        }
+        else{
+            console.log('tui amar bondhu na')
+        }
+
+    }
+
+}
